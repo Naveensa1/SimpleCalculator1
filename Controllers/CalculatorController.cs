634cfd75002a47ab5b0c3d7085dc1a51653a81cd
navee <navee@MSI>
2026-02-01 20:49:16 +0530

    Add project files.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(double number1, double number2, string operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "Add":
+                    result = number1 + number2;
+                    break;
+                case "Subtract":
+                    result = number1 - number2;
+                    break;
+                case "Multiply":
+                    result = number1 * number2;
+                    break;
+                case "Divide":
+                    result = number2 != 0 ? number1 / number2 : 0;
+                    break;
+            }
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
